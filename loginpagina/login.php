471a47f15dcda9ad81aef6f7ff1a9de70ff068d4
dylan <dylan.deblock82574@gmail.com>
2025-01-26 19:06:23 +0100

    Merge branch 'main' of https://github.com/LanderWil/coworking

--- loginpagina/login.php
+++ loginpagina/login.php
@@ -1,31 +1,55 @@
-<?php include('server.php') ?>
+<?php
+// server.php toevoegen voor databaseverbinding
+include('server.php');
+?>
 <!DOCTYPE html>
-<html>
+<html lang="nl">
 <head>
-  <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="https://unpkg.com/normalize.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
-  <div class="header">
-  	<h2>Login</h2>
-  </div>
-	 
-  <form method="post" action="login.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  		<label>Username</label>
-  		<input type="text" name="username" >
-  	</div>
-  	<div class="input-group">
-  		<label>Password</label>
-  		<input type="password" name="password">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
-  	</div>
-  	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
-  	</p>
-  </form>
+<!-- Header (Navigatiebalk) -->
+<header>
+    <div class="container">
+        <a href="../index.html"><img src="../images/Klus_&_co.png" alt="logo" width="140px"></a>
+        <nav>
+            <ul>
+                <li><a href="../">Home</a></li>
+                <li><a href="../jobpagina">Jobpagina</a></li>
+                <li><a href="../contactpagina">Contact</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<!-- Main content -->
+<main>
+    <div class="container">
+        <h1>Inloggen</h1>
+        <p>Vul je inloggegevens in om verder te gaan:</p>
+
+        <form action="login_verwerk.php" method="POST">
+            <label for="username">Gebruikersnaam:</label>
+            <input type="text" id="username" name="username" placeholder="Gebruikersnaam" required>
+
+            <label for="password">Wachtwoord:</label>
+            <input type="password" id="password" name="password" placeholder="Wachtwoord" required>
+
+            <button type="submit">Inloggen</button>
+        </form>
+
+        <p>Nog geen account?
+            <a href="../registreerpagina/registreer.php">Registreer hier</a>
+        </p>
+    </div>
+</main>
+
+
 </body>
 </html>
