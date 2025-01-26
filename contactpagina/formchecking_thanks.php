48fb4bd68e7a9d4d2db4e912b9a3963daaf8bc70
FERRE VAN_BOCKSTAELE <ferrevanbockstaele@gmail.com>
2025-01-26 17:21:04 +0100

    form

--- contactpagina/formchecking_thanks.php
+++ contactpagina/formchecking_thanks.php
@@ -7,10 +7,12 @@ $age = isset($_GET['age']) ? $_GET['age'] : false;
 <!DOCTYPE html>
 <html lang="nl">
 <head>
-    <meta charset="UTF-8">
+	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bedankt!</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Contactpagina</title>
+    <link rel="stylesheet" href="stylesc.css">
+    <link rel="stylesheet" href="https://unpkg.com/normalize.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
